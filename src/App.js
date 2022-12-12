2cde38ee9b1d5efae01cd41c71091566bc69b701
Guilherme Lima Teixeira <guilherme.teixeira@C0143.local>
2022-12-12 17:00:50 -0300

    First commit to project Shortly-app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react"
+import Logo from "./images/bg-boost-desktop.svg"
 
 const App = () => {
   return (
     <div>
+      <img src={Logo} width={500}/>
       <h1>Hello World</h1>
+      <h2>This is the very first project by Gui Rock</h2>
+      <h3>I'm devasted by the fact i can't get a image on this page</h3>
     </div>
   )
-}
+  }
 
 export default App
